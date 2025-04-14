762f3ac9e8747b713db4a5b6c0973cf1abd241a7
kauanmateus <kamateus77@gmail.com>
2025-04-13 21:11:00 -0300

    criei o model contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30, blank=True) # blank = True é pra dizer se o campo é opcional
+    email = models.EmailField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)   # esse timezone.now vai colocar a data automaticamente
+    description = models.TextField(blank=True)
