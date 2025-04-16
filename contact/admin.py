4746a91dc7756ea92f4a0d2fda9e1f4721316052
kauanmateus <kamateus77@gmail.com>
2025-04-16 15:01:07 -0300

    Criando e configurando o model category

--- contact/admin.py
+++ contact/admin.py
@@ -8,3 +8,8 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone'
     # Ordenando os contatos
     ordering = '-id',   # Ordenado com id decrescente
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -11,6 +23,7 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)    
 
 
     def __str__(self) -> str:
